c17a92aa286b5b0b35ce1c5f5e7a62386019786a
Karan <aanvinegi06@gmail.com>
2026-06-02 22:23:31 +0530

    homework

--- Week_1/Day_1(b).java
+++ Week_1/Day_1(b).java
@@ -1,4 +1,4 @@
-lass Solution {
+class Solution {
     public int removeDuplicates(int[] nums) {
         int k=1;
         for(int i = 1;i<nums.length;i++){
---
+++ Week_1/Day_2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+
+        for(int i = 0; i < nums.length; i++) {
+
+            int currentSum = 0;
+
+            for(int j = i; j < nums.length; j++) {
+
+                currentSum = currentSum + nums[j];
+
+                if(currentSum > maxSum) {
+                    maxSum = currentSum;
+                }
+            }
+        }
+
+        return maxSum;
+    }
+}
