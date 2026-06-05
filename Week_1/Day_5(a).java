2f3b08c02c5d946a68dc661a791569590f81161c
Karan <aanvinegi06@gmail.com>
2026-06-05 23:18:03 +0530

    homework

---
+++ Week_1/Day_5(a).java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+         String str = "";
+
+    
+        for(int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if(Character.isLetterOrDigit(ch)) {
+                str = str + Character.toLowerCase(ch);
+            }
+        }
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while(left < right) {
+
+            if(str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    
+}
