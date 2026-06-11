8d89fb87d9bd004e1dec5944d96afd9d1b5d2c3e
Karan <aanvinegi06@gmail.com>
2026-06-11 23:43:33 +0530

    homework

---
+++ Week_1/Week_2/Day_4(a).java
@@ -0,0 +1,20 @@
+lass Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+
+            StringBuilder word = new StringBuilder(words[i]);
+            result.append(word.reverse());
+
+            if (i != words.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+
+    
+}
