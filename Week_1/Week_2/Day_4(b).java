f1ed396f83a08258393c6c60128599fe21813a95
Karan <aanvinegi06@gmail.com>
2026-06-11 23:50:13 +0530

    homework

---
+++ Week_1/Week_2/Day_4(b).java
@@ -0,0 +1,44 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+
+        StringBuilder currentString = new StringBuilder();
+        int k = 0;
+
+        for (char ch : s.toCharArray()) {
+
+            if (Character.isDigit(ch)) {
+                k = k * 10 + (ch - '0');
+            }
+
+            else if (ch == '[') {
+                countStack.push(k);
+                stringStack.push(currentString);
+
+                k = 0;
+                currentString = new StringBuilder();
+            }
+
+            else if (ch == ']') {
+
+                int repeat = countStack.pop();
+                StringBuilder previous = stringStack.pop();
+
+                for (int i = 0; i < repeat; i++) {
+                    previous.append(currentString);
+                }
+
+                currentString = previous;
+            }
+
+            else {
+                currentString.append(ch);
+            }
+        }
+
+        return currentString.toString();
+    }
+
+    
+}
