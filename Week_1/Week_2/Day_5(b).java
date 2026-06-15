abb1928663fdb2327e5bd807fa7492e858bbfeb5
Karan <aanvinegi06@gmail.com>
2026-06-15 23:19:14 +0530

    homework

---
+++ Week_1/Week_2/Day_5(b).java
@@ -0,0 +1,34 @@
+class Solution {
+    public int compress(char[] chars) {
+        int index = 0; 
+        int i = 0;     
+
+        while (i < chars.length) {
+
+            char current = chars[i];
+            int count = 0;
+
+            
+            while (i < chars.length && chars[i] == current) {
+                count++;
+                i++;
+            }
+
+            
+            chars[index++] = current;
+
+            
+            if (count > 1) {
+                String strCount = String.valueOf(count);
+
+                for (char c : strCount.toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+
+        return index;
+    }
+
+    
+}
