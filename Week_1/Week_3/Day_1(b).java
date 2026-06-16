3ff0e88d40730ebdeffb2f2b774defcb1ffb9c68
Karan <aanvinegi06@gmail.com>
2026-06-16 22:46:55 +0530

    homework

---
+++ Week_1/Week_3/Day_1(b).java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+
+            ListNode next = curr.next; // save next node
+
+            curr.next = prev; // reverse link
+
+            prev = curr; // move prev forward
+            curr = next; // move curr forward
+        }
+
+        return prev;
+    }
+
+    
+}
